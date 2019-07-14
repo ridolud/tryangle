8c5ea32e65aaa8363498c62c566f12028a64eaf3
William Santoso <williamsantoso93@yahoo.com>
2019-07-14 19:13:51 +0700

    double tap to preview screen

--- tryangle/Controllers/ComparingController.swift
+++ tryangle/Controllers/ComparingController.swift
@@ -13,6 +13,9 @@ class ComparingController: UIViewController {
     @IBOutlet var imageButtons: [UIButton]!
     @IBOutlet weak var nextButton: UIButton!
     var sendImage: UIImage?
+    var sendTitle: String?
+    var titleNames = ["High", "Eye", "Low"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,21 @@ class ComparingController: UIViewController {
         //change button contentMode to aspect fill
         for button in imageButtons {
             button.imageView?.contentMode = .scaleAspectFill
+            button.addTarget(self, action: #selector(multipleTap(_:event:)), for: .touchDownRepeat)
+        }
+        
+        
+    }
+    
+    @objc func multipleTap(_ sender: UIButton, event: UIEvent) {
+        let touch: UITouch = event.allTouches!.first!
+        if (touch.tapCount == 2) {
+            // do action.
+            sendImage = sender.image(for: .normal)
+            sendTitle = titleNames[sender.tag]
+            if (sendImage != nil) && (sendTitle != nil) {
+                performSegue(withIdentifier: "Preview", sender: nil)
+            }
         }
     }
     
@@ -41,17 +59,26 @@ class ComparingController: UIViewController {
 
     }
     
-    
-    
     @IBAction func nextDidTap(_ sender: Any) {
         if sendImage != nil {
             performSegue(withIdentifier: "TipsShare", sender: nil)
+//            performSegue(withIdentifier: "Preview", sender: nil)
         }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let tipsShareVC = segue.destination as! TipsShareController
-        tipsShareVC.receivedImage = sendImage
+        if segue.identifier == "TipsShare" {
+            let tipsShareVC = segue.destination as! TipsShareController
+            tipsShareVC.receivedImage = sendImage
+        }
+        
+        if segue.identifier == "Preview" {
+            let previewVC = segue.destination as! PreviewController
+            previewVC.receivedImage = sendImage
+            previewVC.receivedTitle = sendTitle
+        }
+        
+        
     }
     
     

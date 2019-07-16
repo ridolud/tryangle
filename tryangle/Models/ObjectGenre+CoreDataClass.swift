6f201f195778f6059e09b4b5325167da151f2bb5
ridolud <faridho.lutfi@gmail.com>
2019-07-16 17:22:02 +0700

    create genre data model and set default entry

---
+++ tryangle/Models/ObjectGenre+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ObjectGenre+CoreDataClass.swift
+//  tryangle
+//
+//  Created by Faridho Luedfi on 16/07/19.
+//  Copyright © 2019 mc2kel7. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ObjectGenre)
+public class ObjectGenre: NSManagedObject {
+
+}

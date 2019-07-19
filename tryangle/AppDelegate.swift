dd24d598823faec735fe9ce2abda71210951f720
ridolud <faridho.lutfi@gmail.com>
2019-07-19 16:52:52 +0700

    update orientation

--- tryangle/AppDelegate.swift
+++ tryangle/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    var restrictRotation:UIInterfaceOrientationMask = .all
+    var restrictRotation:UIInterfaceOrientationMask = .allButUpsideDown
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
